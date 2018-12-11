19f7996a76de8412566240b4f46b2a922c60c573
travisbender <travisbender11@gmail.com>
2018-12-10 20:19:37 -0500

    Changed the Makefile to cpp17, and created a skeletal outline code for how mainthread will run

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@
 
 using namespace std;
 
+int create_thread();
+
+bool clientConnecting = false;
+
+
 int main() {
     vector<Command*> commands{&join, &leave};
 
@@ -16,6 +21,51 @@ int main() {
     cout << "leave matches: " << commands.at(1)->matches("lol") << endl;
     cout << "leave isValid: " << commands.at(1)->isValid("lol") << endl;
     cout << "leave execute: " << commands.at(1)->execute("lol", "no") << endl;
+
+
+
+
+
+    clientConnecting = false;
+
+    cout << "Starting server. Press 'CTRL-C to close the server" << endl;
+    while(true) {
+        /* This is the main thread loop.
+        *  This will loop indefinetly, until the user closes using "CTRL-C"
+        *  This can be changed later to stop under certain conditions
+        */
+
+        if(clientConnecting) {
+            //TODO: Lock mutex
+            cout << "Client connection to server..." << endl;
+
+            //TODO: Add client to global list of users
+
+            //TODO: Spawn Listen thread
+            bool connectionSuccessful = create_thread();
+
+            if (connectionSuccessful)   {cout << "Client successfully connected to sever!" << endl;}
+            else                        {cout << "Client could not connect to server!" << endl;}
+            //TODO: Unlock Mutex
+
+        }
+
+
+    }
+
     return 0;
-    
+
 }
+
+
+
+int create_thread() {
+    //TODO: Spawn a thread
+
+    return false;
+}
+
+
+
+
+
