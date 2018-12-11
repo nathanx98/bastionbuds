dfd869caf93ba0f64559b2eaafd733fa23d0968f
nathanxuan <xuanjiyang09@gmail.com>
2018-12-10 20:33:00 -0500

    it's working now

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-    vector<Command*> commands{&join, &leave};
+    /*vector<Command*> commands{&join, &leave};
 
     cout << "Hello, World!" << endl;
     cout << "join matches: " << commands.at(0)->matches("lol") << endl;
@@ -20,6 +20,7 @@ int main(int argc, char const *argv[]) {
     cout << "leave matches: " << commands.at(1)->matches("lol") << endl;
     cout << "leave isValid: " << commands.at(1)->isValid("lol") << endl;
     cout << "leave execute: " << commands.at(1)->execute("lol", "no") << endl;
+     */
     
     int server_fd;
     int new_socket;
@@ -32,24 +33,40 @@ int main(int argc, char const *argv[]) {
     
     //socket file descriptor
     server_fd = socket(AF_INET,SOCK_STREAM,0);
-//    if(setsockopt(server_fd,SOL_SOCKET,SO_REUSEADDR | SO_REUSEPORT,&opt, sizeof(opt)))
-//    {
-//        perror("setsocketopt");
-//        exit(EXIT_FAILURE);
-//    }
-//    address.sin_family = AF_INET;
-//    address.sin_addr.s_addr = INADDR_ANY;
-//    address.sin_port = htons(8080);
+    if(server_fd < 0)
+    {
+        printf("Error creating server socket");
+        exit(EXIT_FAILURE);
+    }
+    if(setsockopt(server_fd,SOL_SOCKET,SO_REUSEADDR | SO_REUSEPORT,&opt, sizeof(opt)))
+    {
+        perror("setsocketopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
     
     //attach socket to port 8080
-    bind(server_fd,(struct sockaddr*)&address, sizeof(address));
-    listen(server_fd,1024);
+    int bind_sd = bind(server_fd,(struct sockaddr*)&address, sizeof(address));
+    if(bind_sd < 0)
+    {
+        printf("Error binding socket to address\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("Waiting for clients to connect...\n");
+    listen(server_fd,5);
+    
     new_socket = accept(server_fd, (struct sockaddr *)&address,(socklen_t*)&addrlen);
+    
     valread = read(new_socket,buffer,1024);
     printf("%s/n",buffer);
     send(new_socket,hello,strlen(hello),0);
     printf("Hello message sent\n");
+    
+    
+    
+    
+    
     return 0;
-    
-    
 }
